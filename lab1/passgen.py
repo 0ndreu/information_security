d4a982f9cf0ae0fedf0dc828023d9cca0505f570
0ndreu <andrey.artyushov@yandex.ru>
2019-05-21 15:57:15 +0300

    1st try lab1

--- lab1/passgen.py
+++ lab1/passgen.py
@@ -1,26 +1,36 @@
 import random as r
 
-length = int(input('enter the length of the password '))
-password = ''
-numbs = '1234567890'
-lower_let = 'abcdefghigklmnopqrstuvyxwz'
-upper_let = 'ABCDEFGHIGKLMNOPQRSTUVYXWZ'
+while True:
+    try:
+        length = int(input('Enter the length of the password: '))
+    except ValueError:
+        continue
+    password = ''
+    numbs = '1234567890'
+    lower_let = 'abcdefghigklmnopqrstuvyxwz'
+    upper_let = 'ABCDEFGHIGKLMNOPQRSTUVYXWZ'
 
-pass_list =''
+    pass_list =''
 
-tmp = input('Will we use upper-case? ')
-if tmp == 'y':
-    pass_list += upper_let
+    tmp = input('Will we use upper-case?(y/n) ')
+    if tmp == 'y':
+        pass_list += upper_let
 
-tmp = input('Will we use lower-case? ')
-if tmp == 'y':
-    pass_list += lower_let
+    tmp = input('Will we use lower-case?(y/n) ')
+    if tmp == 'y':
+        pass_list += lower_let
 
-tmp = input('Will we use numbers? ')
-if tmp == 'y':
-    pass_list += numbs
+    tmp = input('Will we use numbers?(y/n) ')
+    if tmp == 'y':
+        pass_list += numbs
 
+    if len(pass_list) == 0:
+        print()
+        continue
 
-for i in range(length):
-    password += r.choice(pass_list)
-print (password)
+    for i in range(length):
+        password += r.choice(pass_list)
+    
+    print()
+    print (password)
+    print()
